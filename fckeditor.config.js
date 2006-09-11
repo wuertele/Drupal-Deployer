41fbb2a2547727d4a77918e7764d76b5b0a12f94
ontwerpwerk <ontwerpwerk@72383.no-reply.drupal.org>
2006-09-11 07:31:42 +0000

    Added the php source protected code according to http://drupal.org/node/82895

--- fckeditor.config.js
+++ fckeditor.config.js
@@ -16,6 +16,10 @@ FCKConfig.ToolbarSets["DrupalBasic"] = [
 	['FontFormat','-','Bold','Italic','-','OrderedList','UnorderedList','-','Link','Unlink', 'Image']
 ] ;
 
-// uncomment this line if you want to enable php code in your content
-//FCKConfig.ProtectedSource.Add( /<\?[\s\S]*?\?>/g ) ;	// PHP style server side code
+// Protect PHP code tags (<?...?>) so FCKeditor will not break them when
+// switching from Source to WYSIWYG.
+// Uncomment this line doesn't mean the user will not be able to type PHP
+// code in the source. This kind of prevention must be done in the server side
+// (as does Drupal), so just leave this line as is.
+FCKConfig.ProtectedSource.Add( /<\?[\s\S]*?\?>/g ) ;	// PHP style server side code
 
