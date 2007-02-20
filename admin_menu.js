962c472be8e8412410fcca800269dfe9801fb623
sun <sun@unleashedmind.com>
2007-02-20 16:07:54 +0000

    #112389 Fixed IE / Safari support.

---
+++ admin_menu.js
@@ -0,0 +1,17 @@
+/**
+ * Suckerfish Dropdowns, www.htmldog.com
+ *
+ * IE fix.
+ */
+sfHover = function() {
+	sfEls = document.getElementById("admin_menu").getElementsByTagName("LI");
+	for (var i=0; i<sfEls.length; i++) {
+		sfEls[i].onmouseover=function() {
+			this.className+=" sfhover";
+		}
+		sfEls[i].onmouseout=function() {
+			this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
+		}
+	}
+}
+if (window.attachEvent) window.attachEvent("onload", sfHover);
