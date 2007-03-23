b83cbb52b885af4390d2196e368dfed70c9f4f6a
Stella Power <stella@66894.no-reply.drupal.org>
2007-03-23 10:18:26 +0000

    added new jquery functions for disabling parts of the configuration forms depending on which options are selected.

--- faq.js
+++ faq.js
@@ -1,15 +1,69 @@
 if (Drupal.jsEnabled) {
   $(document).ready(function () {
+    // hide/show answer to question
     $('dd.faq_answer').hide();
     $("dt.faq_question").click(function() {
       $(this).next("dd.faq_answer").toggle();
     });
 
 
+    // hide/show q/a for a category
     $('div.faq_qa_hide').hide();
     $(".faq_qa_header").click(function() {
       $(this).next("div.faq_qa_hide").toggle();
     });
+
+
+
+    // handle faq_category_settings_form
+    faq_display_handler();
+    questions_top_handler();
+    categories_inline_handler();
+    $("input[@name=display]").bind("click", faq_display_handler);
+    $("input[@name=category_display]").bind("click", categories_inline_handler);
+
+
   });
 }
 
+function faq_display_handler(event) {
+  if ($("input[@name=display]:checked").val() == "questions_inline"
+    || $("input[@name=display]:checked").val() == "questions_top") {
+    $("input[@name=use_teaser]").removeAttr("disabled");
+    $("input[@name=more_link]").removeAttr("disabled");
+    $("input[@name=back_to_top]").removeAttr("disabled");
+  }
+  else {
+    $("input[@name=use_teaser]").attr("disabled", "disabled");
+    $("input[@name=more_link]").attr("disabled", "disabled");
+    $("input[@name=back_to_top]").attr("disabled", "disabled");
+  }
+}
+
+function questions_top_handler(event) {
+  $("input[@name=faq_display]").val() == "questions_top" ?
+    $("input[@name=group_questions_top]").removeAttr("disabled"):
+    $("input[@name=group_questions_top]").attr("disabled", "disabled");
+
+  $("input[@name=faq_display]").val() == "questions_top" ?
+    $("input[@name=answer_category_name]").removeAttr("disabled"):
+    $("input[@name=answer_category_name]").attr("disabled", "disabled");
+}
+
+function categories_inline_handler(event) {
+  if ($("input[@name=faq_display]").val() == "questions_top") {
+    $("input[@name=category_display]:checked").val() == "categories_inline" ?
+      $("input[@name=group_questions_top]").removeAttr("disabled"):
+      $("input[@name=group_questions_top]").attr("disabled", "disabled");
+  }
+  else {
+    $("input[@name=group_questions_top]").attr("disabled", "disabled");
+  }
+
+  $("input[@name=category_display]:checked").val() == "categories_inline" ?
+    $("input[@name=hide_sub_categories]").attr("disabled", "disabled"):
+    $("input[@name=hide_sub_categories]").removeAttr("disabled");
+  $("input[@name=category_display]:checked").val() == "categories_inline" ?
+    $("input[@name=show_cat_sub_cats]").attr("disabled", "disabled"):
+    $("input[@name=show_cat_sub_cats]").removeAttr("disabled");
+}
