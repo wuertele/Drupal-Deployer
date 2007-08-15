ed78104bf517f421332d73034315e561f03f91d6
Wim Leers <work@wimleers.com>
2007-08-15 21:13:01 +0000

    Added a configuration option: (horizontal space) filling tabs.

---
+++ panels_tabs.js
@@ -0,0 +1,23 @@
+// $Id$
+
+if (Drupal.jsEnabled) {
+	$(document).ready(function(){
+		var $e;
+		var id;
+		var settings = new Array;
+		
+		for (id in Drupal.settings.panels_tabs['filling_tabs']) {
+			settings['filling_tabs'] = Drupal.settings.panels_tabs['filling_tabs'][id];
+
+			$e = $("div."+ id +" ul li");
+			$e.each(function() {
+				if (settings['filling_tabs'] == 1) {
+		   		$(this).css('width', (99 / $e.length) +'%').css('overflow', 'hidden');
+				}
+				else {
+					$(this).css('min-width', (99 / $e.length) +'%');
+				}
+		 	});
+		}
+	});
+}
