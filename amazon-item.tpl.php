637a3b8f6bad0e0a6f38442eb2d7ba966ad85db8
The Great Git Migration <tggm@no-reply.drupal.org>
2008-06-05 08:08:15 +0000

    This commit was manufactured as part of Drupal's Great Git Migration to create branch 'DRUPAL-6--1'.
    Cherrypick from master 2008-06-05 08:08:14 UTC Jeff Eaton <eaton@lullabot.com> 'Major work on the theme portions of the module. tpl.php files are now being used to display items, and templates can be implemented in a very focused fashion, printing out custom data for specific product types but falling back to default templates in other cases.':
        amazon-item.tpl.php
        amazon_media/amazon-item-dvd-default.tpl.php

---
+++ amazon-item.tpl.php
@@ -0,0 +1,4 @@
+<div class="<?php print $classes; ?>">
+<?php print $image; ?>
+<h3><?php print l($item['title'], $item['detailpageurl']); ?></h3>
+</div>
---
+++ amazon_media/amazon-item-dvd-default.tpl.php
@@ -0,0 +1,7 @@
+<div class="<?php print $classes; ?>">
+<?php print $smallimage; ?>
+<h3><?php print l($title, $detailpageurl); ?> (<?php print $theatricalreleaseyear; ?>)</h3>
+<div><strong><?php print t('Director'); ?>:</strong> <?php print $director; ?></div>
+<div><strong><?php print t('Rating'); ?>:</strong> <?php print $audiencerating; ?></div>
+<div><strong><?php print t('Rating'); ?>:</strong> <?php print $runningtime . ' ' . t('minutes'); ?></div>
+</div>
