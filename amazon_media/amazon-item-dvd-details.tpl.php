85d659f4eb93f7f70bac4c7ee49dee0fa80755bc
Jeff Eaton <eaton@lullabot.com>
2008-06-26 05:57:24 +0000

    Fixing a long-standing bug with multiple creators (authors, actors, etc).

--- amazon_media/amazon-item-dvd-details.tpl.php
+++ amazon_media/amazon-item-dvd-details.tpl.php
@@ -2,6 +2,6 @@
 <?php print $smallimage; ?>
 <div><strong><?php print l($title, $detailpageurl); ?></strong> (<?php print $theatricalreleaseyear; ?>)</div>
 <div><strong><?php print t('Director'); ?>:</strong> <?php print $director; ?></div>
+<div><strong><?php print t('Starring'); ?>:</strong> <?php print $actor; ?></div>
 <div><strong><?php print t('Rating'); ?>:</strong> <?php print $audiencerating; ?></div>
-<div><strong><?php print t('Running time'); ?>:</strong> <?php print t('@time minutes', array('@time' => $runningtime)); ?></div>
 </div>
