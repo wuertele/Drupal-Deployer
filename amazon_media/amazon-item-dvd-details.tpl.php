6d9593595dadcbcd10d65646dab1ebe7e913be97
Jeff Eaton <eaton@lullabot.com>
2008-06-26 05:10:58 +0000

    Template fixes, and some additional handling for ugly product type codes that Amazon tags onto edge case products.

--- amazon_media/amazon-item-dvd-details.tpl.php
+++ amazon_media/amazon-item-dvd-details.tpl.php
@@ -3,5 +3,5 @@
 <div><strong><?php print l($title, $detailpageurl); ?></strong> (<?php print $theatricalreleaseyear; ?>)</div>
 <div><strong><?php print t('Director'); ?>:</strong> <?php print $director; ?></div>
 <div><strong><?php print t('Rating'); ?>:</strong> <?php print $audiencerating; ?></div>
-<div><strong><?php print t('Running time'); ?>:</strong> <?php print t('%time minutes', array('%time' => $runningtime)); ?></div>
+<div><strong><?php print t('Running time'); ?>:</strong> <?php print t('@time minutes', array('@time' => $runningtime)); ?></div>
 </div>
