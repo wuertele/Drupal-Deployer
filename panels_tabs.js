17d1b07aeef1501bfa269689fede4c7205bf0201
Wim Leers <work@wimleers.com>
2008-07-05 21:55:45 +0000

    Implemented #270509: Paged views don't have the tab fragment set

--- panels_tabs.js
+++ panels_tabs.js
@@ -2,6 +2,23 @@
 
 if (Drupal.jsEnabled) {
   $(document).ready(function(){ 
+
+    // This function can be used to update the links of a pager to contain
+    // the currently selected fragment. This allows you to click a link of a
+    // pager in a panel, and when the page reloads, the correct tab will
+    // be opened.
+    updateLinks = function(id, fragment) {
+      $('#tabs-'+ id +' .pager a').each(function() {
+        var oldURI = $(this).attr('href');
+        if (oldURI.lastIndexOf('#') > 0) {
+          oldURI = oldURI.substr(0, oldURI.lastIndexOf('#'));
+        }
+        var newURI = oldURI + fragment;
+        $(this).attr('href', newURI);
+      });
+    };    
+
+    // Resize tabs as configured.
     for (var id in Drupal.settings.panelsTabs) {
       var $tabs = $('#tabs-'+ id +' > ul > li');
 
@@ -30,12 +47,30 @@ if (Drupal.jsEnabled) {
             .css('overflow', 'hidden');
           });
           break;
-        
+
         case 'disabled':
         default:
           // Nothing should happen if we 
           break;
       }
+
+      // Update links when a tab is clicked.
+      $('#tabs-'+ id +' > ul > li > a').each(function() {
+        $(this).click(function(_id, fragment) {
+          return function() { updateLinks(_id, fragment); };
+        }(id, $(this).attr('href')));
+      });
     }
+
+    // Update all links on load (automatically find the name of the tabset!).
+    var currentURI = "" + window.location;
+    var fragment = currentURI.substr(currentURI.indexOf('#'));
+    var id = $('a[@href*="'+ fragment +'"]')
+    .parent() // the parent li
+    .parent() // the parent ul
+    .parent() // the parent div (the actual tabset div!)
+    .attr('id')
+    .substr("tabs-".length);
+    updateLinks(id, fragment);
   });
 }
