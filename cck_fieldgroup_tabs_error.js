2c27fe12dc3c89c3924ad99477393f7d6e484665
James Pooton <moonshine@133705.no-reply.drupal.org>
2008-10-01 06:35:48 +0000

    #315604: By Moonshine - Only trigger jQuery error behavior once

--- cck_fieldgroup_tabs_error.js
+++ cck_fieldgroup_tabs_error.js
@@ -4,10 +4,10 @@
  * Attach client side error classes on tabs and select first tab in error
  */
 Drupal.behaviors.cckFieldgroupTabsErrors = function (context) {
-  $('.ui-tabs-nav').children('li').each( function() {
+  $('.ui-tabs-nav:not(.cckFieldgroupTabsErrors-processed)').addClass('cckFieldgroupTabsErrors-processed').children('li').each( function() {
     if ($($(this).find('a').attr('href')).find('div.form-item .error').size()) {
       $(this).addClass('error').addClass('selected');
     }
+    $('#tabs-fieldgroup_tabs > ul').tabs('select', $('.ui-tabs-nav').children('li.error:first').find('a').attr('href'));
   })
-  $('#tabs-fieldgroup_tabs > ul').tabs('select', $('.ui-tabs-nav').children('li.error:first').find('a').attr('href'));
 }
