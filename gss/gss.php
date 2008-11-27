5b31fea2bb23fb3c516b19a9218c31c87041b780
Alberto Paderno <kiam.git@avpnet.org>
2008-11-27 09:40:54 +0000

    Moved the gss.xsl CVS Id tag into a XML comment.

--- gss/gss.php
+++ gss/gss.php
@@ -1,10 +1,10 @@
 <<?php print '?xml version="1.0" encoding="UTF-8"?' ?>>
-<?php /* $Id$ */ ?>
 <xsl:stylesheet version="2.0"
                 xmlns:html="http://www.w3.org/TR/REC-html40"
                 xmlns:sitemap="http://www.sitemaps.org/schemas/sitemap/0.9"
                 xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
   <xsl:output method="html" version="1.0" encoding="iso-8859-1" indent="yes"/>
+  <!-- <?php print '$Id$'?>  -->
   <!-- Root template -->
   <xsl:template match="/">
     <html>
