1b6520d6a8bbf80b8597ead88aa9f3e711f56143
Nicholas Thompson <njt1982@gmail.com>
2009-04-15 12:11:53 +0000

    Some major reworking to the module including new features such as taxonomy and user page titles

--- page_title-admin-settings-form.tpl.php
+++ page_title-admin-settings-form.tpl.php
@@ -1,15 +1,23 @@
 <?php
 $rows = array();
 
-foreach (element_children($form['title']) as $key) {
-  $row = array();
-  $row[] = drupal_render($form['title'][$key]);
-  $row[] = drupal_render($form['pattern'][$key]);
+foreach (element_children($form['pattern']) as $key) {
+  $title = array(
+    '#type' => 'item',
+    '#title' => $form['pattern'][$key]['#title'],
+    '#required' => $form['pattern'][$key]['#required'],
+  );
+  unset($form['pattern'][$key]['#title']);
+
+  $row = array(
+    drupal_render($title),
+    drupal_render($form['pattern'][$key]),
+    isset($form['showfield'][$key .'_showfield']) ? drupal_render($form['showfield'][$key .'_showfield']) : '',
+  );
+
   $rows[] = $row;
 }
 
-print theme('table', array('Page Type', 'Pattern'), $rows);
+print theme('table', array(t('Page Type'), t('Pattern'), t('Show Field')), $rows);
 
 print drupal_render($form);
-
-print '<h2>'. t('Available Tokens') .'</h2>'. theme('token_help');
