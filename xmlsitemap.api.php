1bc15906327fbcd8d0f14e719b86e432249033e9
Dave Reid <dave@davereid.net>
2009-05-09 22:26:41 +0000

    by Dave Reid: Fixed minor cleanups.

--- xmlsitemap.api.php
+++ xmlsitemap.api.php
@@ -71,5 +71,17 @@ function hook_xmlsitemap_link_alter(&$link) {
 }
 
 /**
+ * Alter the list of sitemap engines.
+ */
+function hook_xmlsitemap_engines_alter(&$engines) {
+  $engines['kitten_engine'] = array(
+    'name' => t('Kitten Search'),
+    'url' => 'http://kittens.com/ping?sitemap=[sitemap]',
+    'verify_url' => FALSE,
+    'verify_key' => FALSE,
+  );
+}
+
+/**
  * @} End of "addtogroup hooks".
  */
