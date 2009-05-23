2413f55341ffc2772051ced87992fbaf59f9c45d
Dave Reid <dave@davereid.net>
2009-05-23 18:38:36 +0000

    by Dave Reid: Updated translation strings and added translation template.

--- xmlsitemap.api.php
+++ xmlsitemap.api.php
@@ -86,15 +86,5 @@ function hook_xmlsitemap_link_alter(&$link) {
 }
 
 /**
- * Alter the list of sitemap engines.
- */
-function hook_xmlsitemap_engines_alter(&$engines) {
-  $engines['kitten_engine'] = array(
-    'name' => t('Kitten Search'),
-    'url' => 'http://kittens.com/ping?sitemap=[sitemap]',
-  );
-}
-
-/**
  * @} End of "addtogroup hooks".
  */
