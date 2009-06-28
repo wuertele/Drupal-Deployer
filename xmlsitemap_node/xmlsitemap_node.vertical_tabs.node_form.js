0b0150a766eb54449e9efc2fd422e5594cff3936
Dave Reid <dave@davereid.net>
2009-06-28 17:12:41 +0000

    by Dave Reid: Adding vertical tabs support for the XML sitemap node edit fieldset.

---
+++ xmlsitemap_node/xmlsitemap_node.vertical_tabs.node_form.js
@@ -0,0 +1,9 @@
+// $Id$
+
+Drupal.verticalTabs.xmlsitemap = function() {
+  var vals = [];
+  vals.push(Drupal.t('Included in sitemap'));
+  var priority = $('#edit-xmlsitemap-priority-override').val();
+  vals.push(Drupal.t('@priority priority', { '@priority': priority }));
+  return vals.join(', ');
+}
