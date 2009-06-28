ebb13180a020e8fcf39b5940ab0b460cd4e992f2
Dave Reid <dave@davereid.net>
2009-06-28 21:26:24 +0000

    by Dave Reid: Textual improvements to the vertical tabs text.

--- xmlsitemap_node/xmlsitemap_node.vertical_tabs.node_form.js
+++ xmlsitemap_node/xmlsitemap_node.vertical_tabs.node_form.js
@@ -1,11 +1,13 @@
 // $Id$
 
+// The following line is necessary since our JavaScript will execute before the
+// Vertical tabs code.
 Drupal.verticalTabs = Drupal.verticalTabs || {};
 
 Drupal.verticalTabs.xmlsitemap = function() {
   var vals = [];
-  vals.push(Drupal.t('Included in sitemap'));
+  //vals.push(Drupal.t('Included in sitemap'));
   var priority = $('#edit-xmlsitemap-priority-override').val();
-  vals.push(Drupal.t('@priority priority', { '@priority': priority }));
-  return vals.join(', ');
+  vals.push(Drupal.t('Priority @priority', { '@priority': priority }));
+  return vals.join('<br />');
 }
