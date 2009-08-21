eb75b0966bb55567867b37b8c62f37c829003db2
Dave Reid <dave@davereid.net>
2009-08-21 17:03:08 +0000

    by Dave Reid: Jiggered with translated strings.

--- xmlsitemap_node/xmlsitemap_node.vertical_tabs.node_form.js
+++ xmlsitemap_node/xmlsitemap_node.vertical_tabs.node_form.js
@@ -8,12 +8,12 @@ Drupal.verticalTabs.xmlsitemap = function() {
   // Inclusion select field
   var status = $('#edit-xmlsitemap-status').val();
   var status_text = $("#edit-xmlsitemap-status option[value='" + status + "']").text();
-  vals.push(Drupal.t('Inclusion: @status', { '@status': status_text }));
+  vals.push(Drupal.t('Inclusion: @value', { '@value': status_text }));
 
   // Priority select field
   var priority = $('#edit-xmlsitemap-priority').val();
   var priority_text = $("#edit-xmlsitemap-priority option[value='" + priority + "']").text();
-  vals.push(Drupal.t('Priority: @priority', { '@priority': priority_text }));
+  vals.push(Drupal.t('Priority: @value', { '@value': priority_text }));
 
   return vals.join('<br />');
 }
