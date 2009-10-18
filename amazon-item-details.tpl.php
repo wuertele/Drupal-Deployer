4606f3df91d17e9da300a55caa0794e2a373d10a
Randy Fay <rfay@30906.no-reply.drupal.org>
2009-10-18 21:03:39 +0000

    Minor changes discovered writing documentation. Shouldn't write that stuff. Re-work of the examples module.

--- amazon-item-details.tpl.php
+++ amazon-item-details.tpl.php
@@ -1,5 +1,5 @@
 <div class="<?php print $classes; ?>">
-<?php if ($invalid_asin) { print "<div class='invalid_asin'>This item is no longer valid on Amazon.</div>"; } ?>
+<?php if (!empty($invalid_asin)) { print "<div class='invalid_asin'>This item is no longer valid on Amazon.</div>"; } ?>
 <?php print $smallimage; ?>
 <div><strong><?php print l($title, $detailpageurl); ?></strong></div>
 <div><strong><?php print t('Manufacturer'); ?>:</strong> <?php if (!empty($manufacturer)) { print $manufacturer; } ?></div>
--- amazon-item-thumbnail.tpl.php
+++ amazon-item-thumbnail.tpl.php
@@ -1,4 +1,4 @@
 <div class="<?php print $classes; ?>">
-<?php if ($invalid_asin) { print "<div class='invalid_asin'>This item is no longer valid on Amazon.</div>"; } ?>
+<?php if (!empty($invalid_asin)) { print "<div class='invalid_asin'>This item is no longer valid on Amazon.</div>"; } ?>
 <?php if (!empty($mediumimage)) { print $mediumimage; } ?>
 </div>
