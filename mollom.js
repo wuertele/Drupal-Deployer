143ccdb5b634b4d5aef05f0f1bb3cde1c19c4efe
Dries Buytaert <dries@buytaert.net>
2009-11-14 19:50:29 +0000

    - Patch #625584 by rwohleb: made behaviors context aware.

--- mollom.js
+++ mollom.js
@@ -1,41 +1,45 @@
 // $Id$
 
-Drupal.behaviors.mollom = function() {
+Drupal.behaviors.mollom = function(context) {
   // Add onclick.event handlers for CAPTCHA links:
-  $('a#mollom-audio-captcha').click(getAudioCaptcha);
-  $('a#mollom-image-captcha').click(getImageCaptcha);
+  $('a.mollom-audio-captcha', context).click(getAudioCaptcha);
+  $('a.mollom-image-captcha', context).click(getImageCaptcha);
 }
 
 function getAudioCaptcha() {
+  var context = $(this).parents('.form-item').parent();
+  
   // Extract the Mollom session ID from the form:
-  var mollomSessionId = $("input#edit-mollom-session-id").val();
+  var mollomSessionId = $('input.mollom-session-id', context).val();
 
   // Retrieve an audio CAPTCHA:
   var data = $.get(Drupal.settings.basePath + 'mollom/captcha/audio/' + mollomSessionId,
     function(data) {
      // When data is successfully loaded, replace
      // contents of captcha-div with an audio CAPTCHA:
-     $('a#mollom-captcha').parent().html(data);
+     $('a.mollom-captcha', context).parent().html(data);
 
      // Add an onclick-event handler for the new link:
-     $('a#mollom-image-captcha').click(getImageCaptcha);
+     $('a.mollom-image-captcha', context).click(getImageCaptcha);
    });
    return false;
 }
 
 function getImageCaptcha() {
+  var context = $(this).parents('.form-item').parent();
+  
   // Extract the Mollom session ID from the form:
-  var mollomSessionId = $('input#edit-mollom-session-id').val();
+  var mollomSessionId = $('input.mollom-session-id', context).val();
 
   // Retrieve an image CAPTCHA:
   var data = $.get(Drupal.settings.basePath + 'mollom/captcha/image/' + mollomSessionId,
     function(data) {
      // When data is successfully loaded, replace
      // contents of captcha-div with an image CAPTCHA:
-     $('a#mollom-captcha').parent().html(data);
+     $('a.mollom-captcha', context).parent().html(data);
 
      // Add an onclick-event handler for the new link:
-     $('a#mollom-audio-captcha').click(getAudioCaptcha);
+     $('a.mollom-audio-captcha', context).click(getAudioCaptcha);
    });
    return false;
 }
