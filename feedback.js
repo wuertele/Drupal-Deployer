9c4ae8dda8f4fde5d3968de7d9c3bb26b4cd0943
sun <sun@unleashedmind.com>
2010-01-08 22:21:11 +0000

    by sun: Fixed JS behaviors are executed more than once.

--- feedback.js
+++ feedback.js
@@ -1,41 +1,50 @@
 // $Id$
 
 /**
+ * Attach auto-submit to admin view form.
+ */
+Drupal.behaviors.feedbackAdminForm = function (context) {
+  $('#feedback-admin-view-form:not(.feedback-processed)', context).addClass('feedback-processed').each(function () {
+    $(this).find('fieldset.feedback-messages :input[type="checkbox"]').click(function () {
+      this.form.submit();
+    });
+  });
+};
+
+/**
  * Attach collapse behavior to the feedback form block (once).
  */
-Drupal.behaviors.feedbackForm = function() {
-  $block = $('#block-feedback-form');
-  $block.find('span.feedback-link')
-    .prepend('<span id="feedback-form-toggle">[ + ]</span> ')
-    .css('cursor', 'pointer')
-    .toggle(function() {
-        Drupal.feedbackFormToggle($block, false);
-      },
-      function() {
-        Drupal.feedbackFormToggle($block, true);
-      }
-    );
-  $block.find('form').hide()
-    .find(':input[name="ajax"]').val(1).end()
-    .submit(function() {
-      // Toggle throbber/button.
-      $('#feedback-throbber', this).addClass('throbbing');
-      $('#feedback-submit', this).fadeOut('fast', function() {
-        Drupal.feedbackFormSubmit($(this).parents('form'));
+Drupal.behaviors.feedbackForm = function (context) {
+  $('#block-feedback-form:not(.feedback-processed)', context).addClass('feedback-processed').each(function () {
+    var $block = $(this);
+    $block.find('span.feedback-link')
+      .prepend('<span id="feedback-form-toggle">[ + ]</span> ')
+      .css('cursor', 'pointer')
+      .toggle(function () {
+          Drupal.feedbackFormToggle($block, false);
+        },
+        function() {
+          Drupal.feedbackFormToggle($block, true);
+        }
+      );
+    $block.find('form').hide()
+      .find(':input[name="ajax"]').val(1).end()
+      .submit(function() {
+        // Toggle throbber/button.
+        $('#feedback-throbber', this).addClass('throbbing');
+        $('#feedback-submit', this).fadeOut('fast', function () {
+          Drupal.feedbackFormSubmit($(this).parents('form'));
+        });
+        return false;
       });
-      return false;
-    });
-  $block.show();
-  // Attach auto-submit to admin view form.
-  $('fieldset.feedback-messages :input[type="checkbox"]').click(function() {
-    $(this).parents('form').submit();
+    $block.show();
   });
-}
+};
 
 /**
  * Collapse or uncollapse the feedback form block.
  */
-Drupal.feedbackFormToggle = function($block, enable) {
+Drupal.feedbackFormToggle = function ($block, enable) {
   $block.find('form').slideToggle('medium');
   if (enable) {
     $('#feedback-form-toggle', $block).html('[ + ]');
@@ -43,13 +52,13 @@ Drupal.feedbackFormToggle = function($block, enable) {
   else {
     $('#feedback-form-toggle', $block).html('[ &minus; ]');
   }
-}
+};
 
 /**
  * Collapse or uncollapse the feedback form block.
  */
-Drupal.feedbackFormSubmit = function($form) {
-  $.post($form.get(0).action, $form.serialize(), function(data) {
+Drupal.feedbackFormSubmit = function ($form) {
+  $.post($form.get(0).action, $form.serialize(), function (data) {
     // Collapse the form.
     $('#block-feedback-form').find('.feedback-link').click();
     // Display status message.
@@ -59,12 +68,12 @@ Drupal.feedbackFormSubmit = function($form) {
     $('#feedback-throbber', $form).removeClass('throbbing');
     $('#feedback-submit', $form).show();
     // Blend out status message.
-    window.setTimeout(function() {
-      $('#feedback-status-message').fadeOut('slow', function() {
+    window.setTimeout(function () {
+      $('#feedback-status-message').fadeOut('slow', function () {
         $(this).remove();
       });
     }, 3000);
   }, 'json');
   return false;
-}
+};
 
