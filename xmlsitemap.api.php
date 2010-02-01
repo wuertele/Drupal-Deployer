e7f72855df78d7b6415e1007659d9d74c98bf534
Dave Reid <dave@davereid.net>
2010-02-01 21:43:14 +0000

    by Dave Reid: Backported API documentation and watchdog handling in tests from D7.

--- xmlsitemap.api.php
+++ xmlsitemap.api.php
@@ -19,59 +19,39 @@
 function hook_xmlsitemap_link_info() {
   return array(
     'mymodule' => array(
-      'purge' => TRUE, // A boolean if this link type can be purged during a rebuild.
+      'label' => 'My module',
+      'base table' => 'mymodule',
+      'object keys' => array(
+        // Primary ID key on {base table}
+        'id' => 'myid',
+        // Subtype key on {base table}
+        'bundle' => 'mysubtype',
+      ),
+      'path callback' => 'mymodule_path',
+      'bundle label' => t('Subtype name'),
+      'bundles' => array(
+        'mysubtype1' => array(
+          'label' => t('My subtype 1'),
+          'admin' => array(
+            'real path' => 'admin/settings/mymodule/mysubtype1/edit',
+            'access arguments' => array('administer mymodule'),
+          ),
+          'xmlsitemap' => array(
+            'status' => 1,
+            'priority' => 0.5,
+          ),
+        ),
+      ),
+      'xmlsitemap' => array(
+        // Callback function to take an array of IDs and save them as sitemap
+        // links.
+        'process callback' => '',
+        // Callback function used in batch API for rebuilding all links.
+        'rebuild callback' => '',
+        // Callback function called from the XML sitemap settings page.
+        'settings callback' => '',
+      )
     ),
-  );
-}
-
-/**
- * Retrieve a array of links to include in the sitemap.
- *
- * @return
- *   An array of link arrays with the following keys and values:
- *   - 'type' => The type of link (node, user, kitten, etc.).
- *   - 'id' => The ID of the link ($node->nid, $user->uid, etc.).
- *   - 'loc' => The un-aliased Drupal path to the item.
- *   - 'lastmod' => The UNIX timestmap of when the item was last modified.
- *   - 'changefreq' => The interval, in seconds, between the last set of changes.
- *   - 'priority' => An optional priority value between 0.0 and 1.0.
- */
-function hook_xmlsitemap_links() {
-  $links = array();
-
-  $links[] = array(
-    'type' => 'mymodule',
-    'id' => 1,
-    'loc' => 'mymodule/menu/path',
-    'lastmod' => 346245692,
-    'changefreq' => 4600,
-  );
-
-  return $links;
-}
-
-/**
- * Provide batch information for hook_xmlsitemap_links().
- *
- * It is highly recommended that if your module has a lot of items that could
- * be sitemap links, that you implement this hook.
- *
- * All you need to do to implement this hook is add the required $context
- * information.
- *
- * The optional current value will provide the offset parameter to
- * hook_xmlsitemap_links() and should get records that are greater than this
- * value. The default value is 0.
- *
- * The max (count) value will allow the batch to know when it is finished. This
- * value is required.
- */
-function hook_xmlsitemap_links_batch_info() {
-  return array(
-    'current' => 0,
-    // This value is used to start selecting items (WHERE id > current).
-    'max' => db_result(db_query("SELECT COUNT(id) FROM {mymodule}")),
-    // This should be the total number of items to process.
   );
 }
 
