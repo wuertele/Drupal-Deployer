54e1bcc9e84bfdc53dc3935f545dcaaff94fb8fb
Dries Buytaert <dries@buytaert.net>
2010-02-19 05:50:39 +0000

    - Patch #640450 by sun: audio CAPTCHA was not accepting correct response.

--- mollom.js
+++ mollom.js
@@ -17,46 +17,54 @@ Drupal.behaviors.mollomPrivacy = function (context) {
  * Attach click event handlers for CAPTCHA links.
  */
 Drupal.behaviors.mollomCaptcha = function (context) {
-  $('.mollom-audio-captcha', context).click(getAudioCaptcha);
-  $('.mollom-image-captcha', context).click(getImageCaptcha);
+  $('a.mollom-audio-captcha', context).click(getAudioCaptcha);
+  $('a.mollom-image-captcha', context).click(getImageCaptcha);
 };
 
 function getAudioCaptcha() {
-  var context = $(this).parents('.form-item').parent();
+  var context = $(this).parents('form');
 
   // Extract the Mollom session ID from the form:
   var mollomSessionId = $('input.mollom-session-id', context).val();
 
   // Retrieve an audio CAPTCHA:
-  var data = $.get(Drupal.settings.basePath + 'mollom/captcha/audio/' + mollomSessionId,
-    function(data) {
-      // When data is successfully loaded, replace
-      // contents of captcha-div with an audio CAPTCHA:
-      $('.mollom-captcha', context).parent().html(data);
-
-      // Add an onclick-event handler for the new link:
-      $('.mollom-image-captcha', context).click(getImageCaptcha);
-   });
-   return false;
+  $.getJSON(Drupal.settings.basePath + 'mollom/captcha/audio/' + mollomSessionId,
+    function (data) {
+      if (!(data && data.content)) {
+        return;
+      }
+      // Inject new audio CAPTCHA.
+      $('.mollom-captcha-content', context).parent().html(data.content);
+      // Update session id.
+      $('input.mollom-session-id', context).val(data.session_id);
+      // Add an onclick-event handler for the new link.
+      $('a.mollom-image-captcha', context).click(getImageCaptcha);
+    }
+  );
+  return false;
 }
 
 function getImageCaptcha() {
-  var context = $(this).parents('.form-item').parent();
+  var context = $(this).parents('form');
 
   // Extract the Mollom session ID from the form:
   var mollomSessionId = $('input.mollom-session-id', context).val();
 
   // Retrieve an image CAPTCHA:
-  var data = $.get(Drupal.settings.basePath + 'mollom/captcha/image/' + mollomSessionId,
-    function(data) {
-      // When data is successfully loaded, replace
-      // contents of captcha-div with an image CAPTCHA:
-      $('.mollom-captcha', context).parent().html(data);
-
-      // Add an onclick-event handler for the new link:
-      $('.mollom-audio-captcha', context).click(getAudioCaptcha);
-   });
-   return false;
+  $.getJSON(Drupal.settings.basePath + 'mollom/captcha/image/' + mollomSessionId,
+    function (data) {
+      if (!(data && data.content)) {
+        return;
+      }
+      // Inject new image CAPTCHA.
+      $('.mollom-captcha-content', context).parent().html(data.content);
+      // Update session id.
+      $('input.mollom-session-id', context).val(data.session_id);
+      // Add an onclick-event handler for the new link.
+      $('a.mollom-audio-captcha', context).click(getAudioCaptcha);
+    }
+  );
+  return false;
 }
 
 })(jQuery);
