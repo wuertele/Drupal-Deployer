6a2842534eba8df3f02367e654189009fad08a6d
Earl Miles <merlin@logrus.com>
2010-10-18 17:54:41 +0000

    #920266 by dereine: Typo in cache.inc caused wsods in rare circumstances.

--- plugins/display_renderers/panels_renderer_standard.class.php
+++ plugins/display_renderers/panels_renderer_standard.class.php
@@ -550,14 +550,15 @@ class panels_renderer_standard {
       }
     }
 
+    ctools_include('cleanstring');
     // Pass long the css_id that is usually available.
     if (!empty($pane->css['css_id'])) {
-      $content->css_id = $pane->css['css_id'];
+      $content->css_id = ctools_cleanstring($pane->css['css_id']);
     }
 
     // Pass long the css_class that is usually available.
     if (!empty($pane->css['css_class'])) {
-      $content->css_class = $pane->css['css_class'];
+      $content->css_class = ctools_cleanstring($pane->css['css_class']);
     }
 
     return $content;
