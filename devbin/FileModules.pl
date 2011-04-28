d1d54806f2f5247e3ec401edf8b1c6cc9cf0a20f
David Wuertele <dave+github@wuertele.com>
2011-04-28 13:51:21 -0700

    porting scripts from drupal-deployer-subtree-merge-2

---
+++ devbin/FileModules.pl
@@ -0,0 +1,1 @@
+push @file_modules, { filename => "jquery.ui-1.6.zip", url => "http://jquery-ui.googlecode.com/files/jquery.ui-1.6.zip", unarch => 'unzip jquery.ui-1.6.zip', dirname => undef, path => "drupal-6.x/sites/all/modules/jquery_ui/jquery.ui", wrapper_dir => undef, };
